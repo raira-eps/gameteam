df8cd00666a5324f00c71111954cc0667f3c5fc0
山崎勇輝 <zakisan1999@au.com>
2019-10-10 19:46:03 +0900

    SpeedsterTestにて各シーンの配置イメージ及びシーン移行実装※確認

---
+++ SpeedSterTest/Assets/Script/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject OptionMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InOption()
+    {
+        OptionMenu.SetActive(true);
+    }
+
+    public void OffOption()
+    {
+        OptionMenu.SetActive(false);
+    }
+
+    public void GoStageKettei()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void GoCharaKettei()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
---
+++ SpeedSterTest/Assets/Script/Title.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Title : MonoBehaviour
+{
+    [SerializeField] AudioSource TitleBGM;
+    [SerializeField] GameObject Start_text;
+    [SerializeField] GameObject ClegitMenu;
+    [SerializeField] GameObject ClegitIcon;
+    [SerializeField] AudioClip Sound1;
+    [SerializeField] AudioClip Sound2;
+
+    private float flash = 2.0f;
+    private float time = 0.0f;
+    private bool flashtime = true;
+    void Start()
+    {
+        //TitleBGM.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time >= flash)
+        {
+            if(flashtime == true)
+            {
+                Start_text.SetActive(false);
+                flashtime = false;
+            }
+            else
+            {
+                Start_text.SetActive(true);
+                flashtime = true;
+            }
+
+            time = 0.0f;
+        }
+    }
+
+    public void OpenClegit()
+    {
+        ClegitIcon.SetActive(false);
+        ClegitMenu.SetActive(true);
+    }
+
+    public void CloseClegit()
+    {
+        ClegitMenu.SetActive(false);
+        ClegitIcon.SetActive(true);
+    }
+}
