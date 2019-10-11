3e9d7517012a3794814f03097a0c65ae3b7f7d98
BlackSwordMan <kirito0930@docomo.ne.jp>
2019-10-11 18:03:32 +0900

    Runのスクリプト追加

---
+++ Speed Star/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static public int Score;
+    static public int TrickCount;
+    static public int Time;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
