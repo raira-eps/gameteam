4220d185afd870c59da141a135de2154deba8111
unkimo <araichisato1914@gmail.com>
2019-10-15 17:19:41 +0900

    ポーズ機能の実装

---
+++ Speed Star/Assets/Scripts/Pause.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Pause : MonoBehaviour
+{
+    //インスタンシエイトする際に階層を指定する
+    [SerializeField] private Transform canvastransform;
+
+    [SerializeField] private Text pauseButtonText;
+    //　ポーズした時に表示するUIのプレハブ
+    [SerializeField] private GameObject pauseUIPrefab;
+    //　ポーズUIのインスタンス
+    private GameObject pauseUIInstance;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    public void PauseClick()
+    {
+        if (pauseUIInstance == null)
+        {
+            pauseButtonText.text = "Resume";
+            pauseUIInstance = GameObject.Instantiate(pauseUIPrefab) as GameObject;
+            pauseUIInstance.transform.SetParent(canvastransform, false);
+            Time.timeScale = 0f;
+        }
+        else if (pauseUIInstance != null)
+        {
+            pauseButtonText.text = "ll";
+            Destroy(pauseUIInstance);
+            Time.timeScale = 1f;
+        }
+    }
+}
