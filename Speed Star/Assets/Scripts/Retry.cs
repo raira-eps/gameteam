2bf2e231ea138188ab5377109c63986cdc0436ee
sandou <yatagarasu3421@outlook.jp>
2019-10-28 01:15:41 +0900

    リトライボタン追加※確認

---
+++ Speed Star/Assets/Scripts/Retry.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Retry : MonoBehaviour
+{
+    //山藤制作
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ButtomRetry()
+    {
+        SceneManager.LoadScene(4);
+        //Scene No4 は渋谷
+    }
+}
