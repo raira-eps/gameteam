20bed0f84dedffe3ab042e81ac73c25fc8ff9bc4
sandou <yatagarasu3421@outlook.jp>
2019-11-01 18:09:07 +0900

    リトライのスピード、スコア(途中経過)

--- Speed Star/Assets/Scripts/Retry.cs
+++ Speed Star/Assets/Scripts/Retry.cs
@@ -5,8 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class Retry : MonoBehaviour
 {
+    Result result;
     //山藤制作
+<<<<<<< HEAD
    
+=======
+    // Start is called before the first frame update
+    void Start()
+    {
+        result = FindObjectOfType<Result>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+>>>>>>> Retry2
     /// <summary>
     /// S.Yがやることリスト
     /// 1、スコアの初期化☆彡
