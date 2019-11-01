b8b4e8be49da52beb6a48d14cecc5efbc5210782
山崎勇輝 <zakisan1999@au.com>
2019-11-01 18:39:57 +0900

    キャラクター洗濯時のスクリプト追加

--- Speed Star/Assets/Scripts/CharacterSelect.cs
+++ Speed Star/Assets/Scripts/CharacterSelect.cs
@@ -2,11 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CharacterSelect : MonoBehaviour
 {
+    [SerializeField] GameObject chara1;
     //イニシャルY_Y
 
+    //後ろの色を変える処理
+    public void Start()
+    {
+        Image image = chara1.GetComponent<Image>();
+        switch (PlayerPrefs.GetInt("chara"))
+        {
+            case 0:
+                break;
+            case 1:
+                image.color = new Color(1, 0, 0, 0.5f);
+                break;
+            case 2:
+                image.color = new Color(0, 0, 1, 0.5f);
+                break;
+        }
+    }
     //メインメニューへ移行する
     public void GoMainMenu()
     {
@@ -17,5 +35,19 @@ public class CharacterSelect : MonoBehaviour
     public void GoStageSelect()
     {
         SceneManager.LoadScene(2);
+    }
+
+    public void Chara1()
+    {
+        Image image = chara1.GetComponent<Image>();
+        image.color = new Color(1f, 0f, 0f, 0.5f);
+        PlayerPrefs.SetInt("chara", 1);
+    }
+
+    public void Chara2()
+    {
+        Image image = chara1.GetComponent<Image>();
+        image.color = new Color(0f, 0f, 1f, 0.5f);
+        PlayerPrefs.SetInt("chara", 2);
     }
 }
--- Speed Star/Assets/Scripts/MainMenu.cs
+++ Speed Star/Assets/Scripts/MainMenu.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     //イニシャルY_Y
-
+    [SerializeField] GameObject back;
     //オプション・ヘルプ画面を入れる
     [SerializeField] GameObject OptionMenu;
 
+    //後ろの色を変える処理
+    public void Start()
+    {
+        Image image = back.GetComponent<Image>();
+        switch (PlayerPrefs.GetInt("chara"))
+        {
+            case 0:
+                break;
+            case 1:
+                image.color = new Color(1, 0, 0, 0.5f);
+                break;
+            case 2:
+                image.color = new Color(0, 0, 1, 0.5f);
+                break;
+        }
+    }
     //オプション・ヘルプ画面を呼び出す
     public void InOption()
     {
