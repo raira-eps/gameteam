c5b2c35e2753ad493ef6c71837bd4d111e3723be
unkimo <araichisato1914@gmail.com>
2019-11-15 18:32:40 +0900

    フェンスの見た目を変更

---
+++ Speed Star/Assets/Scripts/Fence.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fence : MonoBehaviour
+{
+    [SerializeField] private GameObject childfence, childfence2;
+    private bool IsShortfence = false;
+    void Start()
+    {
+        Debug.Log(this.gameObject.tag);
+        if (this.gameObject.tag == "ShortFence")
+        {
+            IsShortfence = true;
+        }
+    }
+    void Update()
+    {
+        if (IsShortfence)
+        {
+            childfence.transform.Rotate(new Vector3(0, -90, 0) * Time.deltaTime, Space.World);
+            childfence2.transform.Rotate(new Vector3(0, -90, 0) * Time.deltaTime, Space.World);
+        }
+        else
+        {
+            childfence.transform.Rotate(new Vector3(0, 90, 0) * Time.deltaTime, Space.World);
+            childfence2.transform.Rotate(new Vector3(0, 90, 0) * Time.deltaTime, Space.World);
+        }
+    }
+}
