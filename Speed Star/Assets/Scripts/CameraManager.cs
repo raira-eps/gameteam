4219682e250a655ebb6c7b3408359cad3b492cc3
Sandou <yatagarasu3421@outlook.jp>
2019-12-13 18:23:52 +0900

    プレイヤーの生成修正Script

--- Speed Star/Assets/Scripts/CameraManager.cs
+++ Speed Star/Assets/Scripts/CameraManager.cs
@@ -6,17 +6,17 @@ using UnityEngine;
 //K.R
 public class CameraManager : MonoBehaviour
 {
-    Transform playerPosition;
+    static public Transform playerPosition;
     public static bool areaJump = false;
-
-    void Start()
-    {
-        playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
-    }
 
     void FixedUpdate()
     {
         if (!areaJump) transform.position = Vector3.Lerp(transform.position, playerPosition.position + new Vector3(9.0f, 2.0f, -10.0f), 5.0f * Time.deltaTime);
         else transform.position = Vector3.Lerp(transform.position, playerPosition.position + new Vector3(5.0f, 2.0f, -10.0f), 20.0f * Time.deltaTime);
+    }
+
+    static public void Find()
+    {
+        playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
     }
 }
