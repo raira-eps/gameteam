29f45372c46dd6386e17cb493d9dc0a123634a04
Sandou <yatagarasu3421@outlook.jp>
2019-12-14 17:38:26 +0900

    モルガ、旭ボイス追加、AudioManeger編集

--- Speed Star/Assets/Scripts/AudioManeger.cs
+++ Speed Star/Assets/Scripts/AudioManeger.cs
@@ -15,15 +15,28 @@ public class AudioManeger : MonoBehaviour
         ShortSE,             //ショートSE
         TipsSE,              //チップ獲得SE
         GameOverSE,          //ゲームオーバーSE
-        Landing,              //着地SE
+        Landing,             //着地SE
+        GoalSE               //歓声SE
     }
     public enum BGM
     {
         Asahi,
         Moruga
     }
+    public enum Voice
+    {
+        Jump,              //ジャンプボイス
+        AreaJump,          //エリアジャンプボイス
+        TrickJump,         //トリックボイス
+        Excellent,         //トリック,エクセレント評価ボイス
+        Good,              //トリック,グッド評価ボイス
+        Normal,            //トリック,ノーマル評価ボイス
+        Miss,              //トリック,ミス評価ボイス
+        Damage,            //ダメージボイス
+        Goal,              //ゴール時ボイス
+    }
     static AudioSource[] Audio;
-
+    //SE
     [SerializeField] static AudioClip countdown;          //エアフェンスカウントダウンSE
     [SerializeField] static AudioClip sucusses;           //エアフェンス成功SE
     [SerializeField] static AudioClip but;                //エアフェンス失敗SE
@@ -33,12 +46,34 @@ public class AudioManeger : MonoBehaviour
     [SerializeField] static AudioClip tipSE;              //チップ獲得SE
     [SerializeField] static AudioClip GameOverSE;　　　　 //ゲームオーバーSE
     [SerializeField] static AudioClip landing;            //着地SE
+    [SerializeField] static AudioClip goal;
 
+    //BGM
     [SerializeField] static AudioClip AsahiBGM;
     [SerializeField] static AudioClip MorugaBGM;
 
+    //モルガ声
+    [SerializeField] static AudioClip MorugaJumpVoise;
+    [SerializeField] static AudioClip MorugaAreaJumpVoice;
+    [SerializeField] static AudioClip MorugaTrickJumpVoice;
+    [SerializeField] static AudioClip MorugaExcellentVoice;
+    [SerializeField] static AudioClip MorugaNormalVoice;
+    [SerializeField] static AudioClip MorugaMissVoice;
+    [SerializeField] static AudioClip MorugaDamageVoice;
+    [SerializeField] static AudioClip MorugaGaolVoice;
+
+    //旭声
+    [SerializeField] static AudioClip AsahiJumpVoice;
+    [SerializeField] static AudioClip AsahiAreaJumpVoice;
+    [SerializeField] static AudioClip AsahiTrickJumpVpice;
+    [SerializeField] static AudioClip AsahiExcellentVoice;
+    [SerializeField] static AudioClip AsahiNormalVoice;
+    [SerializeField] static AudioClip AsahiMissVoice;
+    [SerializeField] static AudioClip AsahiDamagiVoice;
+    [SerializeField] static AudioClip AsahiGoalVoice;
     void Start()
     {
+        //----------------------SE-----------------------------------------------------
         Audio = GetComponents<AudioSource>();
         countdown = Resources.Load<AudioClip>("Sounds/SE/Countdown");
         sucusses = Resources.Load<AudioClip>("Sounds/SE/speedster_トリック成功1");
@@ -50,12 +85,12 @@ public class AudioManeger : MonoBehaviour
         GameOverSE = Resources.Load<AudioClip>("Sounds/SE/speedster_ゲームオーバー");
         landing = Resources.Load<AudioClip>("Sounds/SE/speedster_着地SE");
 
+        //----------------------BGM-----------------------------------------------------
         AsahiBGM = Resources.Load<AudioClip>("Sounds/BGM/AsahiPlayBGM");
         MorugaBGM = Resources.Load<AudioClip>("Sounds/BGM/speedster_Morga is IDOL☆");
-    }
 
-    void Update()
-    {
+        //----------------------Voice-----------------------------------------------------
+
 
     }
 
@@ -98,6 +133,10 @@ public class AudioManeger : MonoBehaviour
                     Debug.Log("旭足音");
                 }
                 break;
+            case SE.GoalSE:
+                Debug.Log("歓声");
+                //Audio[0].PlayOneShot(goal);
+                break;
             default:
                 break;
         }
@@ -115,6 +154,62 @@ public class AudioManeger : MonoBehaviour
             default:
                 break;
         }
-
+    }
+    static public void VoiceSE(Voice sound)
+    {
+        if (PlayerPrefs.GetInt("chara") == 1)
+        {
+            //モルガ声　処理
+            switch (sound)
+            {
+                case Voice.Jump:
+                    break;
+                case Voice.AreaJump:
+                    break;
+                case Voice.TrickJump:
+                    break;
+                case Voice.Excellent:
+                    break;
+                case Voice.Good:
+                    break;
+                case Voice.Normal:
+                    break;
+                case Voice.Miss:
+                    break;
+                case Voice.Damage:
+                    break;
+                case Voice.Goal:
+                    break;
+                default:
+                    break;
+            }
+        }
+        else if (PlayerPrefs.GetInt("chara") == 2)
+        {
+            //旭声　処理
+            switch (sound)
+            {
+                case Voice.Jump:
+                    break;
+                case Voice.AreaJump:
+                    break;
+                case Voice.TrickJump:
+                    break;
+                case Voice.Excellent:
+                    break;
+                case Voice.Good:
+                    break;
+                case Voice.Normal:
+                    break;
+                case Voice.Miss:
+                    break;
+                case Voice.Damage:
+                    break;
+                case Voice.Goal:
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
